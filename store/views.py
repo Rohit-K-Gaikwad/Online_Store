d14b614a0cc94a8b8e595833e25f474b82642ae8
Rohit Gaikwad <rgaikwad1998@gmail.com>
2024-11-13 00:23:50 +0530

    added create order and urls

--- store/views.py
+++ store/views.py
@@ -13,3 +13,30 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    @action(detail=False, methods=['post'])
+    def create_order(self, request):
+        serializer = CreateOrderSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.get(id=serializer.validated_data['user_id'])
+            product_ids = serializer.validated_data['product_ids']
+            products = Product.objects.filter(id__in=product_ids)
+
+            total_amount = sum([product.price for product in products])
+
+            # Create order
+            order = Order.objects.create(user=user, total_amount=total_amount)
+            order.products.set(products)
+
+            # Reduce stock
+            for product in products:
+                product.stock -= 1
+                product.save()
+
+            return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
