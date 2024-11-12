b27e255963ef05b584e91f5d2421a938e8058a0c
Rohit Gaikwad <rgaikwad1998@gmail.com>
2024-11-13 00:11:07 +0530

    added createorder serializer

--- store/serializers.py
+++ store/serializers.py
@@ -18,3 +18,20 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class CreateOrderSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    product_ids = serializers.ListField(child=serializers.IntegerField())
+
+    def validate(self, data):
+        product_ids = data['product_ids']
+        products = Product.objects.filter(id__in=product_ids)
+
+        if not products.exists():
+            raise serializers.ValidationError("Invalid product IDs.")
+
+        for product in products:
+            if product.stock < 1:
+                raise serializers.ValidationError(f"Product {product.name} is out of stock.")
+        return data
